ffefda1baeea034fb5a72e4b874332f4221185ef
aidahrahmawati04@gmail.com <aidahrahmawati04@gmail.com>
2023-10-09 11:01:34 +0700

    [WIP] menambah konfigurasi firebase

---
+++ konfigurasi-firebase.js
@@ -0,0 +1,8 @@
+export const konfigurasiFirebase= {
+    apiKey: "AIzaSyBNYqYJVYPTZRIK7pKZ4shbQ4igjseQpLM",
+    authDomain: "insan-cemerlang-d724d.firebaseapp.com",
+    projectId: "insan-cemerlang-d724d",
+    storageBucket: "insan-cemerlang-d724d.appspot.com",
+    messagingSenderId: "630693962922",
+    appId: "1:630693962922:web:a9447f760b858bcf781cd3"
+  };
